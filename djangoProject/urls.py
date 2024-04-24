3adeda0d9793f660b4a0ea57fa9bad7ca0392084
hypeprogrammer <goodmangil@knou.ac.kr>
2024-04-24 18:12:01 +0900

    swagger 설정(api 문서 자동 생성을 위하여)

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from django.urls import path
-from .views import home  # views를 가져오는 부분
+from django.urls import path, re_path
+from .views import home  # 기존의 home 뷰 가져오기
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Sample API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourdomain.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),  # 메인 페이지로 설정
+    path('', home, name='home'),  # 기존 메인 페이지 설정
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
