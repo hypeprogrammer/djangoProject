b8ac9cdd977d961eebcf04294cfde1147800bd35
hypeprogrammer <goodmangil@knou.ac.kr>
2024-04-26 10:16:41 +0900

    models.py CSV파일 모델 정의

---
+++ djangoProject/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Iris(models.Model):
+    sepal_length = models.FloatField()
+    sepal_width = models.FloatField()
+    petal_length = models.FloatField()
+    petal_width = models.FloatField()
+    species = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.species} ({self.sepal_length}, {self.sepal_width}, {self.petal_length}, {self.petal_width})"
